db02211e0a8bfc43038cd033bffe056333c027a5
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-01-23 15:48:07 +0330

    Add new task functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useState } from "react";
-// import { Button } from "@nextui-org/button";
 import { Checkbox } from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
 
 const TaskItem = ({ caption = "", active = false }) => {
   return (
-    <li className="border-t-1 py-3">
+    <li className="border-t-1 py-4">
       <Checkbox defaultSelected radius="sm">
         {caption}
       </Checkbox>
@@ -20,23 +20,72 @@ export default function Home() {
     { caption: "Task 2", done: false },
   ]);
 
+  const [showTaskInput, setShowTaskInput] = useState(true);
+
+  const [taskInput, setTaskInput] = useState("");
+
+  const handleClickonAddTask = () => {
+    const newTask = { caption: taskInput, done: false };
+    setTasks((task) => [...task, newTask]);
+    setShowTaskInput(true);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="w-1/4">
         <div className="flex items-center">
           <div className="w-1/2 h-12">
-            <h1 className="text-2xl mb-6">Tasks</h1>
+            <h1 className="text-2xl mb-6">
+              Tasks{" "}
+              <span className="text-medium align-top text-gray-400">
+                {tasks.length}
+              </span>
+            </h1>
           </div>
-          {/* <div className="w-1/2 h-12 flex justify-end">
-            <Button isIconOnly color="success" radius="full"></Button>
-          </div> */}
         </div>
 
         <ul>
-          {tasks.map((item) => (
-            <TaskItem caption={item.caption} />
+          {tasks.map((item, index) => (
+            <TaskItem caption={item.caption} key={index} />
           ))}
         </ul>
+
+        <div className="border-t-1 py-4">
+          {showTaskInput ? (
+            <div
+              className="flex items-center  cursor-pointer"
+              onClick={() => setShowTaskInput(false)}
+            >
+              <span className="text-3xl text-red-400 font-thin mr-2.5">+</span>
+              <span className="text-gray-400">Add Task</span>
+            </div>
+          ) : (
+            <>
+              <Input
+                type="text"
+                variant="bordered"
+                size="sm"
+                placeholder="Your fucking task tilte"
+                value={taskInput}
+                onChange={(e) => setTaskInput(e.target.value)}
+              />
+              <div className="text-right pt-2">
+                <span
+                  className="mr-3 text-small cursor-pointer"
+                  onClick={() => setShowTaskInput(true)}
+                >
+                  Cancel
+                </span>
+                <span
+                  className="text-small text-red-500 cursor-pointer"
+                  onClick={handleClickonAddTask}
+                >
+                  Add Task
+                </span>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </main>
   );
