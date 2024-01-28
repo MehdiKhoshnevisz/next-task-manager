e3e57fc38c9fd461ae34c964b0e78c0f3cec2ade
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-01-28 11:02:18 +0330

    Create test page

---
+++ src/app/test/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function TestId() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Hello Profile id!</h1>
+    </main>
+  );
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Test() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Hello Test!</h1>
+    </main>
+  );
+}
