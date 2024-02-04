40d5f51fb683d4b49ad5db793703c24c07ed603f
Mehdi Khoshnevis <mkhnsz1996@gmail.com>
2024-02-04 13:43:25 +0330

    Update page.tsx

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function TestId() {
+export default function TestIdPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Hello Profile id!</h1>
